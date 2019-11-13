cdf33f2453a12f7bb5e4ad0562a0915005ec95db
Justus Woolworth <martinvegance@yahoo.com>
2019-11-13 22:32:07 +0800

    Initial Commit

---
+++ ios_open_source_learnig_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_open_source_learnig_project
+//
+//  Created by Justus Woolworth on 11/13/19.
+//  Copyright © 2019 Justus Woolworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios_open_source_learnig_projectTests/ios_open_source_learnig_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_open_source_learnig_projectTests.swift
+//  ios_open_source_learnig_projectTests
+//
+//  Created by Justus Woolworth on 11/13/19.
+//  Copyright © 2019 Justus Woolworth. All rights reserved.
+//
+
+import XCTest
+@testable import ios_open_source_learnig_project
+
+class ios_open_source_learnig_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
