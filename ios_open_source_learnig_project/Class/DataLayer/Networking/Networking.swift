6a4dc00dc602b9d88e032c88d47a59228da55880
justus <martinvegance@yahoo.com>
2019-11-29 15:59:19 +0800

    project structure

---
+++ ios_open_source_learnig_project/Class/DataLayer/Networking/Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Networking.swift
+//  ios_open_source_learnig_project
+//
+//  Created by xiongyongjie on 2019/11/29.
+//  Copyright © 2019 Justus Woolworth. All rights reserved.
+//
+
+import Foundation
---
+++ ios_open_source_learnig_project/Class/Extensions/UIView+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+Extension.swift
+//  ios_open_source_learnig_project
+//
+//  Created by xiongyongjie on 2019/11/29.
+//  Copyright © 2019 Justus Woolworth. All rights reserved.
+//
+
+import Foundation
