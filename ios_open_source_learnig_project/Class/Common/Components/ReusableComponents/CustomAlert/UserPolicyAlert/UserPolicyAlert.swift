53723cfb379ebfc948f8c48052a7dbd51b4e3039
justus <martinvegance@yahoo.com>
2019-12-30 18:18:40 +0800

    new

---
+++ ios_open_source_learnig_project/Class/Common/Components/ReusableComponents/CustomAlert/UserPolicyAlert/UserPolicyAlert.swift
@@ -0,0 +1,9 @@
+//
+//  UserPolicyAlert.swift
+//  ios_open_source_learnig_project
+//
+//  Created by xiongyongjie on 2019/12/30.
+//  Copyright © 2019 Justus Woolworth. All rights reserved.
+//
+
+import Foundation
